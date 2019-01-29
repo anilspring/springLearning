860635a4ad62984a2448487efa557fb283391cb8
anku <anku@mehta>
2019-01-29 08:04:03 +0530

    commit 4

--- demo.com/src/main/java/org/anil/DrawingApp.java
+++ demo.com/src/main/java/org/anil/DrawingApp.java
@@ -1,6 +1,7 @@
 package org.anil;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
@@ -8,9 +9,11 @@ public class DrawingApp {
 	public static void main(String[] args) {
 		//Triangle triangle = new Triangle();
 		
-		ApplicationContext context =   
-			    new ClassPathXmlApplicationContext("spring.xml");
-		Triangle triangle = (Triangle) context.getBean("triangle1");
+	    ApplicationContext context =   
+	    new ClassPathXmlApplicationContext("spring.xml");
+		//AbstractApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		//context.registerShutdownHook();
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
 		
 
