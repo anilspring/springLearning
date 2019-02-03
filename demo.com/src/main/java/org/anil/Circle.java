ae4ff82b9c7ef40ae2e6f1aa09d1db9c02f419c4
anku <anku@mehta>
2019-02-03 23:11:44 +0530

    commit10

--- demo.com/src/main/java/org/anil/Circle.java
+++ demo.com/src/main/java/org/anil/Circle.java
@@ -1,8 +1,13 @@
 package org.anil;
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+@Controller
 public class Circle implements Shape {
      private Point center;
      
@@ -17,7 +22,7 @@ public class Circle implements Shape {
 	}
 	/*@Autowired
 	@Qualifier("circleRelated")*/
-	@Resource(name="pointC")
+	@Resource
 	public void setCenter(Point center) {
 		this.center = center;
 	}
--- demo.com/src/main/java/org/anil/PostConstruct.java
+++
@@ -1,5 +0,0 @@
-package org.anil;
-
-public @interface PostConstruct {
-
-}
