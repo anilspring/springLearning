ebf89e9906434bdac8081cb20fc8d90a08ed50b4
anku <anku@mehta>
2019-02-03 21:56:22 +0530

    commit9

--- demo.com/src/main/java/org/anil/Circle.java
+++ demo.com/src/main/java/org/anil/Circle.java
@@ -1,21 +1,34 @@
 package org.anil;
-
-import org.springframework.beans.factory.annotation.Required;
+import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Circle implements Shape {
      private Point center;
+     
      public void draw() {
     	 System.out.println("Drawing Circle");
     	System.out.println("circle: point is:(" + center.getX()+ " , " +center.getY()+ ")");
     			
-    	}
+   	}
 
 	public Point getCenter() {
 		return center;
 	}
-	@Required
+	/*@Autowired
+	@Qualifier("circleRelated")*/
+	@Resource(name="pointC")
 	public void setCenter(Point center) {
 		this.center = center;
+	}
+	@PostConstruct
+	public void initializeCircle() {
+		System.out.println("Init of circle");
+	}
+	
+	@PostConstruct
+	public void destroyCircle() {
+		System.out.println("destroy of circle");
 	}
 	
 }
---
+++ demo.com/src/main/java/org/anil/PostConstruct.java
@@ -0,0 +1,5 @@
+package org.anil;
+
+public @interface PostConstruct {
+
+}
